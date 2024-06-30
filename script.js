d7b4e1166ffb71241105b365d980a4d6301e4644
Snwqqq <stanislav.cards@gmail.com>
2024-06-30 23:27:48 +0300

    Add reset grid function

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 const gridHolder = document.querySelector('.grid-holder');
 const gridSize = 16;
+const resetGridBut = document.querySelector('.grid-reset');
 let currentGridSize =0;
 let cellGridSize;
 
-for(let i = 0;i<gridSize*gridSize;i++){
-    if(gridSize!==currentGridSize){
-     currentGridSize=gridSize;
-    }
-    let div = document.createElement('div');
-    div.classList.add('grid-cell');
-    div.id=i;
-    div.style.height = `${100/gridSize}%`;
-    div.style.minWidth = `${100/gridSize}%`;   
-    gridHolder.appendChild(div);
-}
+createGrid();
 
 gridHolder.addEventListener("mouseover",(event)=>{
     let target = event.target;
     let div = target;
     div.style.backgroundColor = 'white';
 })
+
+
+resetGridBut.addEventListener('click',()=>{
+    deleteGrid(gridHolder);
+    createGrid();
+})
+
+
+
+
+
+function deleteGrid(element){
+    while(element.firstChild) element.removeChild(element.firstChild);
+}
+
+function createGrid(){
+    for(let i = 0;i<gridSize*gridSize;i++){
+        if(gridSize!==currentGridSize){
+         currentGridSize=gridSize;
+        }
+        let div = document.createElement('div');
+        div.classList.add('grid-cell');
+        div.id=i;
+        div.style.height = `${100/gridSize}%`;
+        div.style.minWidth = `${100/gridSize}%`;   
+        gridHolder.appendChild(div);
+    }
+}
